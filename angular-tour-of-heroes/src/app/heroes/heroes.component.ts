acf09db6c1a2566c918d00423cbb4e02a2ac3242
pornpavit <toompornpavit@gmail.com>
2019-03-13 16:54:15 +0700

    4. Service

--- angular-tour-of-heroes/src/app/heroes/heroes.component.ts
+++ angular-tour-of-heroes/src/app/heroes/heroes.component.ts
@@ -3,6 +3,8 @@ import { Hero } from '../hero' ;
 
 import { HEROES } from '../mock-heroes';
 
+import { HeroService } from '../hero.service';
+
 @Component({
   selector: 'app-heroes',
   templateUrl: './heroes.component.html',
@@ -11,24 +13,23 @@ import { HEROES } from '../mock-heroes';
 })
 export class HeroesComponent implements OnInit {
 
-  hero: Hero ={
-    id: 1,
-    name: 'Windstorm'
-  };
-
   selectedHero: Hero;
+
+  heroes: Hero[];
+
+  constructor(private heroService: HeroService) { }
+  
   onSelect(hero: Hero): void {
     this.selectedHero = hero;
   }
 
-  heroes = HEROES;
-
-  constructor() { }
-
-  
+  getHeroes(): void {
+    this.heroService.getHeroes()
+    .subscribe(heroes => this.heroes = heroes);
+  }
 
   ngOnInit() {
-    
+    this.getHeroes();
   }
 
 }
